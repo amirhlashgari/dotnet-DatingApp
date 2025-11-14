113f9b022e27ca90a0be8558a5aa18a889f5de87
Amirhosein Lashkari <amirhosseinlashgari.al@gmail.com>
2025-11-14 23:36:44 +0330

    feat(S4): using jsoninput(DTO)

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,23 +2,26 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AccountController(ApplicationDbContext context) : BaseApiController
 {
     [HttpPost("register")] // POST:api/account/register
-    public async Task<ActionResult<AppUser>> Register(string email, string displayName, string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
-        // now with this type it accepts query params as service inputs, should create DTO and object
+        if (await EmailExists(registerDto.Email)) return BadRequest("Email taken");
+
         using var hmac = new HMACSHA512(); // USING means the parameter should be disposed after it is used and we should not be worried about its garbage collection 
         var user = new AppUser
         {
-            DisplayName = displayName,
-            Email = email,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
 
@@ -26,5 +29,10 @@ public class AccountController(ApplicationDbContext context) : BaseApiController
         await context.SaveChangesAsync();
 
         return user;
+    }
+
+    private async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
     }
 }
